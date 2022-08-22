9d98fd238a7363fea1ba8df306730df9f63a48b3
kguevarapv <camila.apaza@ucsm.edu.pe>
2022-08-22 09:39:07 -0500

    Clase Principal que influye el ingreso de un numeor por teclado y lo muestra

---
+++ Principal.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int vida, rp = 0;
+		boolean f1 = false;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Numero? ");
+		vida = sc.nextInt();
+		System.out.println("Numero ingresado: " + vida);
+	}
+}
